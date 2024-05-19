53e007a9b43cdf40b92cd439065d46bc2921fb10
Robby J <54921185+RobbiDev@users.noreply.github.com>
2024-05-19 19:24:27 -0400

    New Expirences

--- components/JobCard.jsx
+++ components/JobCard.jsx
@@ -1,52 +1,36 @@
-import CFA from '@/assets/cfa.png'
 import Image from 'next/image'
 
-import data from '@/data/jobs.json'
+export default function JobCard({ company, location, date, year, image,position,  bullet = [] }) {
 
-
-export default function JobCard({ title, location, date, year, image, fruits }) {
-
-	console.log(fruits)
+	const logo = require(`../assets/${image}`)
 
 	return (
 		<>
 			<div className={"row-span-2 card-bg p-5 text-white "} >
 				<header className='flex flex-row space-x-5'>
 					<div className=' w-[15%] sm:w-[5%]'>
-						<Image src={image} />
+						<Image src={logo}  alt={`${company} logo`} />
 					</div>
 					<div className='w-[75%] sm:w-[95%]'>
 						<div className='flex flex-row justify-between'>
-							<p class="text-xs text-zinc-100 pb-2">{date}</p>
-							<p class="text-xs text-zinc-500">{year}</p>
+							<p className="text-xs text-zinc-100 pb-2">{date}</p>
+							<p className="text-xs text-zinc-500">{year}</p>
 						</div>
-						<a href="https://www.virtuollis.com/" className='text-xl font-bold text-zinc-100 group-hover:text-white font-display'>{title}</a>
-						<p class="text-xs text-zinc-500 pt-2">{location}</p>
+						<a href="https://www.virtuollis.com/" className='text-xl font-bold text-zinc-100 group-hover:text-white font-display'> {position} @ {company}</a>
+						<p className="text-xs text-zinc-500 pt-2">{location}</p>
 
-						<ul className='mt-5 flex flex-col space-y-5'>
-
-							<ul>
-								{fruits.map((fruit, index) => (
-									<li key={index}>{fruit}</li>
-								))}
-							</ul>
-
-							{/* <li className='flex flex-col'>
-								<h3 class="text-lg font-semibold text-gray-900 dark:text-white">{roles[1].title}</h3>
-								<time class="text-sm font-normal text-zinc-500">hi</time>
-							</li> */}
-
-
-
-
-						</ul>
+						<ol className='mt-5 flex flex-col space-y-5 text-sm md:text-lg'>
+							{bullet.map((point, index) => (
+								<li key={index} className='flex flex-col'>
+									{point}
+								</li>
+							))}
+						</ol>
 					</div>
 				</header>
 
 				<div>
-
 				</div>
-
 			</div>
 		</>
 	)
--- pages/experience/index.js
+++ pages/experience/index.js
@@ -1,68 +1,84 @@
-import Image from 'next/image'
-import { Inter, Signika, Konkhmer_Sleokchher } from 'next/font/google'
-import Head from 'next/head'
-import JobCard from '@/components/JobCard'
-import CFA from '@/assets/cfa.png'
-import Virtuollis from '@/assets/virtuollis.svg'
-import Link from 'next/link'
+// General Imports
+import Head from 'next/head';
+import JobCard from '@/components/JobCard';
+import jobsData from '@/data/jobs.json';
 
-import data from '@/data/jobs.json'
+// Define fonts with specific configurations
+import { Konkhmer_Sleokchher, Roboto } from 'next/font/google';
+const Konkhmer = Konkhmer_Sleokchher({ weight: ["400"], subsets: ['latin'] });
+const PrimaryFont = Roboto({ weight: ["400"], subsets: ['latin'] });
 
-const Konkhmer = Konkhmer_Sleokchher({ weight: ["400"], subsets: ['latin'] })
-const arrayData = ['Apple', 'Banana', 'Cherry'];
+/**
+ * Home component for displaying categorized job listings.
+ */
 export default function Home() {
-
+    // Filter jobs based on categories
+    const softwareJobs = jobsData.filter(job => job.category === 'software');
+    const generalJobs = jobsData.filter(job => job.category === 'general');
 
     return (
-        
-        <main className={`${Konkhmer.className}`}>
+        <div className={`${Konkhmer.className}`}>
             <Head>
                 <title>Projects | Robert Johnson</title>
                 <meta name="description" content="My Portfolio Website" />
             </Head>
 
             <main className={`container mx-auto ${Konkhmer.className} px-5 pb-10`}>
-
+                {/* Header section */}
                 <header className='flex flex-col md:flex-row text-lg py-10 items-center sm:justify-between text-white border-b-white '>
-
                     <div className='text-gray-400'>
                         <a href='/' >making/things/better<span className='gradient'>/experience</span></a>
                     </div>
-
-
                     <nav className='flex flex-row justify-between space-x-5 text-gray-400'>
                         <a href='/'>Home</a>
                         <a href='/projects'>Projects</a>
                         <a href='/contact'>Contact</a>
                     </nav>
-
-
                 </header>
 
+                {/* Main content section */}
                 <section className='flex flex-col space-y-5 text-white text-2xl'>
-                    <div>
-                        <h1>Software Engineering</h1>
-                        <div class="w-full mt-5 h-px  bg-zinc-800"></div>
-
-                        <div className='flex flex-col'>
-                            <JobCard date={"January 16th, 2023"} year={"9 mos"} title={"Virtuollis LLC"} location={"Ontario, Canada · Remote"} image={Virtuollis}>
-                            <h1>BOH Supervisor</h1>
-							<p class="text-xs text-zinc-500 ">Jan 2023 - Present</p>
-                            </JobCard>
+                    <div className="flex space-y-5 flex flex-col">
+                        {/* Software Engineering Jobs */}
+                        <div>
+                            <h1 className='mb-5'>Software Engineering</h1>
+                            <div className='flex flex-col space-y-5'>
+                                {softwareJobs.map((job, index) => (
+                                    <JobCard
+                                        key={index}
+                                        date={job.date}
+                                        year={job.time}
+                                        company={job.company}
+                                        location={job.location}
+                                        image={job.logo}
+                                        position={job.position}
+                                        bullet={job.bulletpoints}
+                                    />
+                                ))}
+                            </div>
                         </div>
 
-                    </div>
-                    <div>
-                        <h1>General Jobs</h1>
-                        <div class="w-full mt-5 h-px bg-zinc-800"></div>
-                        <div className='flex flex-col space-y-5'>
-                            {data.map(job => <JobCard date={job.date} year={job.time} title={job.company} location={job.location} image={CFA} fruits={arrayData} /> )}
+                        {/* General Jobs */}
+                        <div>
+                            <h1 className='mb-5'>General Jobs</h1>
+                            <div className='flex flex-col space-y-5'>
+                                {generalJobs.map((job, index) => (
+                                    <JobCard
+                                        key={index}
+                                        date={job.date}
+                                        year={job.time}
+                                        company={job.company}
+                                        location={job.location}
+                                        image={job.logo}
+                                        position={job.position}
+                                        bullet={job.bulletpoints}
+                                    />
+                                ))}
+                            </div>
                         </div>
                     </div>
-
                 </section>
             </main>
-
-        </main>
-    )
+        </div>
+    );
 }
