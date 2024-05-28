192c567fa0c5f33cb21700cefa278b67e0828917
Robby J <54921185+RobbiDev@users.noreply.github.com>
2024-05-27 21:06:16 -0400

    hn

--- components/JobCard.jsx
+++ components/JobCard.jsx
@@ -1,31 +1,26 @@
 import Image from 'next/image'
 
-export default function JobCard({ company, location, date, year, image,position,  bullet = [] }) {
+export default function JobCard({ company, location, date, year, image,position,  summary }) {
 
 	const logo = require(`../assets/${image}`)
 
 	return (
 		<>
-			<div className={"row-span-2 card-bg p-5 text-white "} >
+			<div className={"row-span-2 card-bg p-5 text-white shadow-xl"} >
 				<header className='flex flex-row space-x-5'>
-					<div className=' w-[15%] sm:w-[5%]'>
-						<Image src={logo}  alt={`${company} logo`} />
-					</div>
-					<div className='w-[75%] sm:w-[95%]'>
+					<div className=''>
+						
 						<div className='flex flex-row justify-between'>
 							<p className="text-xs text-zinc-100 pb-2">{date}</p>
 							<p className="text-xs text-zinc-500">{year}</p>
 						</div>
-						<a href="https://www.virtuollis.com/" className='text-xl font-bold text-zinc-100 group-hover:text-white font-display'> {position} @ {company}</a>
+
+						<a className='text-xl font-bold text-zinc-100 group-hover:text-white font-display'> {position} @ {company}</a>
 						<p className="text-xs text-zinc-500 pt-2">{location}</p>
 
-						<ol className='mt-5 flex flex-col space-y-5 text-sm md:text-lg'>
-							{bullet.map((point, index) => (
-								<li key={index} className='flex flex-col'>
-									{point}
-								</li>
-							))}
-						</ol>
+						<p className='mt-5 flex flex-col space-y-5 font-md text-zinc-300'>
+							{summary}
+						</p>
 					</div>
 				</header>
 
--- pages/experience/index.js
+++ pages/experience/index.js
@@ -12,12 +12,10 @@ const PrimaryFont = Roboto({ weight: ["400"], subsets: ['latin'] });
  * Home component for displaying categorized job listings.
  */
 export default function Home() {
-    // Filter jobs based on categories
-    const softwareJobs = jobsData.filter(job => job.category === 'software');
-    const generalJobs = jobsData.filter(job => job.category === 'general');
 
     return (
-        <div className={`${Konkhmer.className}`}>
+        <div>
+
             <Head>
                 <title>Projects | Robert Johnson</title>
                 <meta name="description" content="My Portfolio Website" />
@@ -25,7 +23,7 @@ export default function Home() {
 
             <main className={`container mx-auto ${Konkhmer.className} px-5 pb-10`}>
                 {/* Header section */}
-                <header className='flex flex-col md:flex-row text-lg py-10 items-center sm:justify-between text-white border-b-white '>
+                <header className='flex flex-col md:flex-row text-lg py-10 items-center sm:justify-between text-white border-b-white'>
                     <div className='text-gray-400'>
                         <a href='/' >making/things/better<span className='gradient'>/experience</span></a>
                     </div>
@@ -36,48 +34,58 @@ export default function Home() {
                     </nav>
                 </header>
 
-                {/* Main content section */}
-                <section className='flex flex-col space-y-5 text-white text-2xl'>
-                    <div className="flex space-y-5 flex flex-col">
-                        {/* Software Engineering Jobs */}
-                        <div>
-                            <h1 className='mb-5'>Software Engineering</h1>
-                            <div className='flex flex-col space-y-5'>
-                                {softwareJobs.map((job, index) => (
-                                    <JobCard
-                                        key={index}
-                                        date={job.date}
-                                        year={job.time}
-                                        company={job.company}
-                                        location={job.location}
-                                        image={job.logo}
-                                        position={job.position}
-                                        bullet={job.bulletpoints}
-                                    />
-                                ))}
+                <section className='flex flex-col md:flex-row md:space-x-5 '>
+                    <section className='flex max-h-screen flex-col xl:w-1/2'>
+                        <div className="flex space-y-5 flex flex-col max-h-screen">
+                            <h1 className='text-white'>ABOUT</h1>
+                            <p className='font-md text-zinc-300'>
+                                Experienced professional with a passion
+                                for making things better through problem-
+                                solving and system design. Utilizes
+                                technical expertise and innovative
+                                thinking to drive growth and efficiency.
+                                Committed to the value of "Making it
+                                better" in all endeavors.
+                            </p>
+                        </div>
+                        <div className='flex flex-col space-y-5 py-10'>
+                            <h1 className=' text-white'>SKILLS</h1>
+                            <div className='text-zinc-300'>
+                            <p>hi</p>
+                            <p>hi</p>
+                            <p>hi</p>
+                            <p>hi</p>
                             </div>
                         </div>
+                    </section>
 
-                        {/* General Jobs */}
-                        <div>
-                            <h1 className='mb-5'>General Jobs</h1>
-                            <div className='flex flex-col space-y-5'>
-                                {generalJobs.map((job, index) => (
-                                    <JobCard
-                                        key={index}
-                                        date={job.date}
-                                        year={job.time}
-                                        company={job.company}
-                                        location={job.location}
-                                        image={job.logo}
-                                        position={job.position}
-                                        bullet={job.bulletpoints}
-                                    />
-                                ))}
+                    {/* Main content section */}
+                    <section className='flex flex-col space-y-5 text-white'>
+                        <div className="flex space-y-5 flex flex-col">
+                            {/* Software Engineering Jobs */}
+                            <div>
+                                <h1 className='mb-5'>EXPERIENCE</h1>
+                                <div className='flex flex-col space-y-5'>
+                                    {jobsData.map((job, index) => (
+                                        <JobCard
+                                            key={index}
+                                            date={job.date}
+                                            year={job.time}
+                                            company={job.company}
+                                            location={job.location}
+                                            image={job.logo}
+                                            position={job.position}
+                                            summary={job.summary}
+                                        />
+                                    ))}
+                                </div>
                             </div>
+
+
                         </div>
-                    </div>
+                    </section>
                 </section>
+
             </main>
         </div>
     );
