ab0cb17b13ccc16c4f7b50ef0074d6760adcdab1
Robby J <54921185+RobbiDev@users.noreply.github.com>
2024-07-03 18:22:58 -0400

    Update email-handler.js

--- pages/api/email-handler.js
+++ pages/api/email-handler.js
@@ -37,9 +37,9 @@ export default async function handler(req, res) {
       res.status(200).json({ success: true });
     } catch (error) {
       console.error(error);
-      res.status(500).json({ error: 'Error sending email!' });
+      res.status(500).json({ error: 'Error sending email' });
     }
   } else {
-    res.status(405).json({ error: 'Method not allowed!' });
+    res.status(405).json({ error: 'Method not allowed' });
   }
 }
