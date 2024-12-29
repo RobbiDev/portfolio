3305cf159dc99c3d71dc0fc33907bf270cd5ac3b
Robby J <54921185+RobbiDev@users.noreply.github.com>
2024-12-29 01:53:42 -0500

    1.0.3
    Fixed Landing Page Bugs

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,26 @@ import Image from "next/image";
 import RMATA from "../assets/RMATA.svg"; // Replace this with the path to your image
 
 export default function Home() {
+  
+  const [dimensions, setDimensions] = useState({ width: 200, height: 200 });
+
+  useEffect(() => {
+    // Update dimensions based on window size
+    const handleResize = () => {
+      const width = window.innerWidth;
+      if (width < 640) {
+        setDimensions({ width: 150, height: 150 }); // Small screens
+      } else if (width < 768) {
+        setDimensions({ width: 200, height: 200 }); // Medium screens
+      }
+    };
+
+    // Set initial size and listen for resize events
+    handleResize();
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+  
   return (
     <main className="Konkhmer">
       <Head>
@@ -13,13 +33,19 @@ export default function Home() {
       </Head>
 
       {/* Main Container */}
-      <div className="flex justify-center items-center h-screen overflow-hidden relative home-bg">
+      <div className="flex justify-center items-center h-screen overflow-hidden relative sm:home-bg">
         {/* Logo and Lines */}
         <div className="relative flex flex-col items-center">
           {/* Logo */}
           <div className="flex flex-col items-center space-y-3">
             <div className="relative">
-              <Image src={RMATA} alt="Robert Johnson" width={200} height={200} className="drop-shadow-md shadow-black rounded-xl"/>
+            <Image
+        src={RMATA}
+        alt="Robert Johnson"
+        width={dimensions.width}
+        height={dimensions.height}
+        className="rounded-xl drop-shadow-md shadow-black"
+      />
               {/* First Line */}
               <div className="absolute h-6 bg-[#F7941D] border-white left-full top-[calc(50%-25px)] animate-expand-line"></div>
 
@@ -31,7 +57,7 @@ export default function Home() {
 
             </div>
 
-            <div className="flex flex-row items-center space-x-5 font-geist text-white font-bold text-lg md:text-xl">
+            <div className="flex flex-row items-center space-x-5 Konkhmer text-white font-bold text-sm sm:text-xl">
               <a href="/projects">projects</a>
               <a href="/contact">contact</a>
               <a href="/experience">about</a>
